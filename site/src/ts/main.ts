21d8ebc47b256e2d75c553968bd3b2c0d6b22f86
AysonAudio <contact@aysonaudio.dev>
2024-10-09 19:58:49 -0700

    refactor: remove elem cache from spa.ts

--- site/src/ts/main.ts
+++ site/src/ts/main.ts
@@ -4,17 +4,24 @@ import * as Spa from "./spa.js";
 // ########################## INIT SPA BUTTONS ######################### //
 // ##################################################################### //
 document.addEventListener("DOMContentLoaded", () => {
-    let cache = Spa.GetElemCache();
+    // Get elements
+    const tabBtns: NodeListOf<HTMLAnchorElement> = document.querySelectorAll(
+        "#tabs > .tabs > ul > li > a"
+    );
+    const portfolioPageBtns: NodeListOf<HTMLAnchorElement> =
+        document.querySelectorAll("[id^='btn-portfolio-']");
+    const projectPageBtns: NodeListOf<HTMLAnchorElement> =
+        document.querySelectorAll("[id^='btn-project-']");
+    const reelBtns: NodeListOf<HTMLAnchorElement> =
+        document.querySelectorAll("[id^='reel-btn-']");
 
     // Init buttons
-    for (const tabBtn of cache.tabBtns) Spa.initTabBtn(tabBtn);
-
-    for (const pageBtn of cache.portfolioPageBtns) Spa.initPageBtn(pageBtn);
-    for (const pageBtn of cache.projectPageBtns) Spa.initPageBtn(pageBtn);
-
-    for (const reelBtn of cache.reelBtns) Spa.initReelBtn(reelBtn);
+    for (const tabBtn of tabBtns) Spa.initTabBtn(tabBtn);
+    for (const pageBtn of portfolioPageBtns) Spa.initPageBtn(pageBtn);
+    for (const pageBtn of projectPageBtns) Spa.initPageBtn(pageBtn);
+    for (const reelBtn of reelBtns) Spa.initReelBtn(reelBtn);
 
     // In each tab section, default to first page
-    cache.portfolioPageBtns[0]?.click();
-    cache.projectPageBtns[0]?.click();
+    portfolioPageBtns[0]?.click();
+    projectPageBtns[0]?.click();
 });
--- site/src/ts/spa.ts
+++ site/src/ts/spa.ts
@@ -2,74 +2,6 @@
 // ############################ HELPER FUNCS ########################### //
 // ##################################################################### //
 
-/** Cache of elems manipulated by SPA */
-type ElemCache = {
-    ///// ----------------------------------------------------------------
-    ///// SPA elements that are hidden / unhidden when buttons are clicked
-    ///// ----------------------------------------------------------------
-
-    /** index.html.haml */
-    sections: {
-        portfolios: HTMLElement;
-        projects: HTMLElement;
-    };
-    /** index.html.haml */
-    portfolioPages: NodeListOf<HTMLElement>;
-    /** index.html.haml */
-    projectPages: NodeListOf<HTMLElement>;
-    /** sections/_reel-gallery.haml */
-    reelVidBoxes: NodeListOf<HTMLDivElement>;
-
-    ///// ---------------------------------------
-    ///// SPA Buttons that hide / unhide elements
-    ///// ---------------------------------------
-
-    /** parts/_tab.haml */
-    tabBtns: NodeListOf<HTMLAnchorElement>;
-    /** sections/_nav.haml */
-    portfolioPageBtns: NodeListOf<HTMLAnchorElement>;
-    /** sections/_nav.haml */
-    projectPageBtns: NodeListOf<HTMLAnchorElement>;
-    /** sections/_reel-gallery.haml */
-    reelBtns: NodeListOf<HTMLAnchorElement>;
-
-    ///// -----------------------------------------------------------
-    ///// Videos that stop playing when their SPA container is closed
-    ///// -----------------------------------------------------------
-
-    /** sections/_reel-gallery.haml */
-    reelVids: NodeListOf<HTMLVideoElement>;
-};
-
-/**
- * Returns a cache of elems manipulated by SPA.
- * Created in an anonymous factory function.
- */
-export const GetElemCache: () => ElemCache = (() => {
-    let cache: ElemCache = {
-        sections: {
-            portfolios: document.querySelector("#portfolios"),
-            projects: document.querySelector("#projects"),
-        },
-        portfolioPages: document.querySelectorAll("[id^='portfolio-']"),
-        projectPages: document.querySelectorAll("[id^='project-']"),
-        reelVidBoxes: document.querySelectorAll("[id^='reel-vid-box-']"),
-
-        tabBtns: document.querySelectorAll("#tabs > .tabs > ul > li > a"),
-        portfolioPageBtns: document.querySelectorAll("[id^='btn-portfolio-']"),
-        projectPageBtns: document.querySelectorAll("[id^='btn-project-']"),
-        reelBtns: document.querySelectorAll("[id^='reel-btn-']"),
-
-        reelVids: document.querySelectorAll("[id^='reel-vid-box-'] > video"),
-    };
-
-    // Make cache immutable
-    Object.freeze(cache);
-
-    // Return function that returns cache
-    return () => cache;
-})();
-
 // ##################################################################### //
 // ############################# INIT FUNCS ############################ //
 // ##################################################################### //
@@ -81,24 +13,34 @@ export const GetElemCache: () => ElemCache = (() => {
  *   Pause all reel videos
  */
 export const initTabBtn = (tabBtn: HTMLAnchorElement) => {
-    let cache = GetElemCache();
+    // Get elements
+    const sections = {
+        portfolios: document.querySelector("#portfolios") as HTMLElement,
+        projects: document.querySelector("#projects") as HTMLElement,
+    };
+    const tabBtns: NodeListOf<HTMLAnchorElement> = document.querySelectorAll(
+        "#tabs > .tabs > ul > li > a"
+    );
+    const reelVids: NodeListOf<HTMLVideoElement> = document.querySelectorAll(
+        "[id^='reel-vid-box-'] > video"
+    );
 
     // Get tag of section to unhide. Extracts it from tab button class
     const targetSectionTag = tabBtn.className;
 
     tabBtn.addEventListener("click", () => {
         // Use CSS classes to hide / unhide content sections
-        for (const id in cache.sections) {
-            const section: HTMLElement = cache.sections[id];
+        for (const id in sections) {
+            const section: HTMLElement = sections[id];
             if (id === targetSectionTag) section.classList.remove("is-hidden");
             else section.classList.add("is-hidden");
         }
         // Use CSS classes to set active tab button
-        for (const _tabBtn of cache.tabBtns)
+        for (const _tabBtn of tabBtns)
             _tabBtn.parentElement.classList.remove("is-active");
         tabBtn.parentElement.classList.add("is-active");
         // Pause all reel videos
-        for (const elem of cache.reelVids) elem.pause();
+        for (const elem of reelVids) elem.pause();
     });
 };
 
@@ -109,18 +51,39 @@ export const initTabBtn = (tabBtn: HTMLAnchorElement) => {
  *   Pause all reel videos
  */
 export const initPageBtn = (pageBtn: HTMLAnchorElement) => {
-    let cache = GetElemCache();
+    // Get elements
+    const pages = {
+        portfolioPages: document.querySelectorAll(
+            "[id^='portfolio-']"
+        ) as NodeListOf<HTMLElement>,
+        projectPages: document.querySelectorAll(
+            "[id^='project-']"
+        ) as NodeListOf<HTMLElement>,
+    };
+
+    const pageBtns = {
+        portfolioPageBtns: document.querySelectorAll(
+            "[id^='btn-portfolio-']"
+        ) as NodeListOf<HTMLAnchorElement>,
+        projectPageBtns: document.querySelectorAll(
+            "[id^='btn-project-']"
+        ) as NodeListOf<HTMLAnchorElement>,
+    };
+
+    const reelVids: NodeListOf<HTMLVideoElement> = document.querySelectorAll(
+        "[id^='reel-vid-box-'] > video"
+    );
 
     // Get id of page to unhide. Extracts it from page button id
     const targetPageId = pageBtn.id.slice("btn-".length);
     const targetPageType = targetPageId.slice(0, targetPageId.indexOf("-"));
     const targetPageNum = targetPageId.slice(targetPageId.indexOf("-") + 1);
-    const targetPageBtnList = targetPageType + "PageBtns";
+    const targetPageBtnsList = targetPageType + "PageBtns";
     const targetPagesList = targetPageType + "Pages";
 
     pageBtn.addEventListener("click", () => {
         // Use CSS classes to hide / unhide pages
-        for (const elem of cache[targetPagesList] as NodeListOf<HTMLElement>) {
+        for (const elem of pages[targetPagesList] as NodeListOf<HTMLElement>) {
             if (elem.id === targetPageId)
                 for (const child of elem.children)
                     child.classList.remove("is-hidden");
@@ -129,7 +92,7 @@ export const initPageBtn = (pageBtn: HTMLAnchorElement) => {
                     child.classList.add("is-hidden");
         }
         // Update screen reader metadata
-        for (const _pageBtn of cache[targetPageBtnList]) {
+        for (const _pageBtn of pageBtns[targetPageBtnsList]) {
             _pageBtn.classList.remove("is-current");
             _pageBtn.setAttribute(
                 "aria-label",
@@ -141,7 +104,7 @@ export const initPageBtn = (pageBtn: HTMLAnchorElement) => {
         pageBtn.setAttribute("aria-label", "Page " + (targetPageNum + 1));
         pageBtn.setAttribute("aria-current", "page");
         // Pause all reel videos
-        for (const elem of cache.reelVids) elem.pause();
+        for (const elem of reelVids) elem.pause();
     });
 };
 
@@ -152,24 +115,32 @@ export const initPageBtn = (pageBtn: HTMLAnchorElement) => {
  *   Pause all reel videos
  */
 export const initReelBtn = (reelBtn: HTMLAnchorElement) => {
-    let cache = GetElemCache();
+    // Get elements
+    const reelBtns: NodeListOf<HTMLAnchorElement> =
+        document.querySelectorAll("[id^='reel-btn-']");
+    const reelVidBoxes: NodeListOf<HTMLDivElement> = document.querySelectorAll(
+        "[id^='reel-vid-box-']"
+    );
+    const reelVids: NodeListOf<HTMLVideoElement> = document.querySelectorAll(
+        "[id^='reel-vid-box-'] > video"
+    );
 
     // Get id of video to unhide. Extracts uuid substring from reel button id.
     const uuid = reelBtn.id.slice("reel-btn-".length);
     const boxId = "reel-vid-box-" + uuid;
 
     reelBtn.addEventListener("click", () => {
-        // Use CSS classes to hide / unhide reel video boxes
-        for (const elem of cache.reelVidBoxes) {
-            if (elem.id === boxId) elem.classList.remove("is-hidden");
-            else elem.classList.add("is-hidden");
-        }
         // Use CSS classes to set active menu button
-        for (const elem of cache.reelBtns) {
+        for (const elem of reelBtns) {
             if (elem.id === reelBtn.id) elem.classList.add("is-active");
             else elem.classList.remove("is-active");
         }
+        // Use CSS classes to hide / unhide reel video boxes
+        for (const elem of reelVidBoxes) {
+            if (elem.id === boxId) elem.classList.remove("is-hidden");
+            else elem.classList.add("is-hidden");
+        }
         // Pause all reel videos
-        for (const elem of cache.reelVids) elem.pause();
+        for (const elem of reelVids) elem.pause();
     });
 };
